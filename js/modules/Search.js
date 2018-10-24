9ed9bd7075af11ea6ca147cbcb7e5736c9dff088
SamanthaChia <samantha1999@live.com.sg>
2018-10-24 18:58:20 +0800

    added key bindings

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -13,17 +13,28 @@ class Search {
     // events aka listener
     events(){
         this.openButton.on("click", this.openOverlay.bind(this));
-
         this.closeButton.on("click",this.closeOverlay.bind(this));
+        $(document).on("keyup", this.keyPressedFunction.bind(this));
     }
 
     //methods (function,action, etc)
     openOverlay(){
         this.searchOverlay.addClass("search-overlay--active");
+        $("body").addClass("body-no-scroll");
     }
 
     closeOverlay(){
         this.searchOverlay.removeClass("search-overlay--active");
+        $("body").removeClass("body-no-scroll");
+    }
+
+    keyPressedFunction(key){
+        if(key.keyCode == 83){
+            this.openOverlay();
+        }
+        else if(key.keyCode ==27){
+            this.closeOverlay();
+        }
     }
 
 }
