b91f2f85103543cfc7d2ccb55e1b6631088cdb81
SamanthaChia <samantha1999@live.com.sg>
2018-10-29 16:31:15 +0800

    basic design for login and register page

--- functions.php
+++ functions.php
@@ -62,4 +62,24 @@ function subscriberNoAdminBar(){
        show_admin_bar(false);
     }
 }
+
+//Customize Login Screen
+add_filter('login_headerurl','headerUrl');
+
+function headerUrl() {
+    return esc_url(site_url('/'));
+}
+
+add_action('login_enqueue_scripts', 'loginCSS');
+
+function loginCSS() {
+    wp_enqueue_style('customfont', '//fonts.googleapis.com/css?family=Slabo+27px');
+    wp_enqueue_style('style2', get_stylesheet_uri());
+}
+
+add_filter('login_headertitle','loginTitle');
+
+function loginTitle(){
+    return get_bloginfo('name');
+}
 ?>
